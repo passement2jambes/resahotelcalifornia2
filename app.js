e06c0d961093232ac22f10f4ca0250f72ef9525f
imran <pdmpppdm@gmail.com>
2025-09-30 14:37:43 +0200

    accueil

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
 // inclure les dépendances et middlewares
-const mysql = require('mysql2')
-const express = require('express')
-const ejs = require('ejs')
+const mysql = require('mysql2');
+const express = require('express');
+const ejs = require('ejs');
+const iniparser = require('iniparser');
 
 // activer les dépendances
-let configDB = iniparser.parseSync('./DB.ini')
-let app = express()
-app.set('view engine', 'ejs')
-app.use(express.static('views'))
-app.use(express.static('public'))
+let configDB = iniparser.parseSync('./db.ini');
+let app = express();
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(express.static('public'));
 let mysqlconnexion = mysql.createConnection({
     host: configDB['dev']['host'],
     user: configDB['dev']['user'],
     password: configDB['dev']['password'],
     database: configDB['dev']['database']
-})
+});
+
+//vérification de connexion à la base de donnée
+mysqlconnexion.connect((err) => {
+    if (!err) console.log('BDD connectée.')
+    else console.log('BDD connexion échouée \n Erreur: ' + JSON.stringify(err))
+});
+
+// activer le middleware et lancer l'application sur le port 3060
+app.use(express.json());
+app.use(express.urlencoded());
+
+
+// utiliser les routers
+app.get('/', (req, res) => {
+    res.render('accueil');
+});
+
+
+
+app.listen(3010, () => console.log('Bienvenue à l\'Hotel California !'))
